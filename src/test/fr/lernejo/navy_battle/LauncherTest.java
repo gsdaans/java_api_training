2f4334062356bed33d05be75f3201386fa88d4c8
gsdaans <gs.daans@gmail.com>
2022-03-30 19:23:19 +0200

    Adding tests for Launcher

--- src/test/fr/lernejo/navy_battle/LauncherTest.java
+++ src/test/fr/lernejo/navy_battle/LauncherTest.java
@@ -8,12 +8,23 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class LauncherTest {
     private static int testPort = 1111;
+    private static String testUrl = "localhost";
+
 
     @Test
     public void correctPort() {
         Assertions.assertDoesNotThrow( () -> Launcher.main(new String[] { Integer.toString(testPort) }));
     }
 
+    @Test
+    public void correctPortAndUrl() {
+        Assertions.assertDoesNotThrow( () -> Launcher.main(new String[] { Integer.toString(testPort+1), "localhost" }));
+    }
+
+    @Test
+    public void correctTwoArgs() {
+        Assertions.assertDoesNotThrow( () -> Launcher.main(new String[] { Integer.toString(testPort+2), testUrl }));
+    }
     @Test
     public void noArgs() {
         Throwable exception = assertThrows(IllegalArgumentException.class, () -> Launcher.main(new String[] {}));
