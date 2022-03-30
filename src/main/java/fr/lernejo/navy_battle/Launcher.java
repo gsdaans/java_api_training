eb04c85528855b1dd4581c024fd4349eafa985f8
gsdaans <gs.daans@gmail.com>
2022-03-30 05:33:05 +0200

    Fixing Post and Server parameter

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -7,8 +7,8 @@ public class Launcher {
         if (args.length < 1) {
             throw new IllegalArgumentException("Please specify a port number");
         }
-        else if (args.length > 1) {
-            Server server = new Server(Integer.parseInt(args[0]), args[1]);
+        else {
+            Server server = new Server(Integer.parseInt(args[0]), "localhost");
             server.createServer();
         }
     }
--- src/main/java/fr/lernejo/navy_battle/Post.java
+++ src/main/java/fr/lernejo/navy_battle/Post.java
@@ -2,7 +2,6 @@ package fr.lernejo.navy_battle;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
-import java.nio.charset.StandardCharsets;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -16,7 +15,6 @@ public class Post implements HttpHandler {
   public void handle(HttpExchange httpExchange) throws IOException {
         if (httpExchange.getRequestMethod().equals("POST")) {
             String body = "{\"id\":\" " + this.port + "\", \"url\":\"http://localhost:" + this.port + "\", \"message\":\"May the code win\"}";
-            String messageReceived = new String(httpExchange.getRequestBody().readAllBytes(), StandardCharsets.UTF_8);
             httpExchange.sendResponseHeaders(202, body.getBytes().length);
             try (OutputStream os = httpExchange.getResponseBody()){
                 os.write(body.getBytes());}
